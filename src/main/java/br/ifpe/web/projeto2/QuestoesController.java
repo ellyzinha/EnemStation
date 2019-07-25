f541a6c8f3841724d9a8749787b75cbd0d9cd03f
IgorLAP <ilap1036@gmail.com>
2019-07-25 16:16:40 -0300

    pesquisar questões

--- src/main/java/br/ifpe/web/projeto2/QuestoesController.java
+++ src/main/java/br/ifpe/web/projeto2/QuestoesController.java
@@ -1,5 +1,7 @@
 package br.ifpe.web.projeto2;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +11,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.ifpe.web.projeto2.DAO.AlternativaDAO;
 import br.ifpe.web.projeto2.DAO.AssuntoDAO;
 import br.ifpe.web.projeto2.DAO.DificuldadeDAO;
+import br.ifpe.web.projeto2.DAO.DisciplinaDAO;
 import br.ifpe.web.projeto2.DAO.QuestoesDAO;
-import br.ifpe.web.projeto2.Model.Alternativa;
-import br.ifpe.web.projeto2.Model.Assunto;
 import br.ifpe.web.projeto2.Model.Questoes;
 import br.ifpe.web.projeto2.service.QuestoesService;
 
@@ -27,57 +30,63 @@ public class QuestoesController {
 	
 	@Autowired
 	private DificuldadeDAO dificuldadeRep;
-
+	
+	@Autowired
+	private AlternativaDAO alternativasRep;
+	
 	@Autowired
 	private QuestoesDAO questoesRep;
-	@Autowired
-	private QuestoesService questoesService;
+	
 	@Autowired
 	private AssuntoDAO assuntoRep;
+	
+	@Autowired
+	private DisciplinaDAO disciplinaRep;
+	
+	@Autowired
+	private QuestoesService questoesService;
 	
 	@GetMapping("/adicionando_questoes")
 	public ModelAndView exibirFormQuestoes(Questoes questoes) {
 		ModelAndView mv = new ModelAndView("adicionando_questoes");		
 		mv.addObject("listaDificuldade", this.dificuldadeRep.findAll(Sort.by("descricao")));
+		mv.addObject("listaAlternativas", this.alternativasRep.findAll(Sort.by("descricao")));
 		mv.addObject("listaAssunto", this.assuntoRep.findAll(Sort.by("descricao")));
 		mv.addObject("questoes", questoes);
 		return mv;
 	}
 	
-	@GetMapping("/alternativa")
-	public ModelAndView exibirFormAlternativa(Alternativa alternativa) {
-		ModelAndView mv = new ModelAndView("questoes-alternativa");
-		mv.addObject("alternativa", alternativa);
-		mv.addObject("listaQuestao", this.questoesRep.findAll());
-		return mv;
-	}
-	
-	@GetMapping("/exibir_questoes")
-	public ModelAndView questoes (Questoes questoes) {
-		ModelAndView mv = new ModelAndView("questoesport");
-		mv.addObject("exibirQuestao", this.questoesRep.findAll());
+	@GetMapping("/questoes")
+	public ModelAndView exibirQuestoes(Questoes questoes) {
+		ModelAndView mv = new ModelAndView("questoes");		
+		mv.addObject("listaAssunto", this.assuntoRep.findAll(Sort.by("descricao")));
+		mv.addObject("questoes", questoes);
 		return mv;
 	}
 	
 	
-	
-	
-	@PostMapping("/salvarAlternativa")
-	public String salvarAlternativa (@Valid @ModelAttribute Alternativa alternativa) {
-		questoesService.criarAlternativa(alternativa);
-		return "redirect:/alternativa";
-	}
+	//@PostMapping("/salvarQuestoes")
+	//public ModelAndView salvarFuncionario(@Valid Questoes questoes, BindingResult br) {
+	//	if(br.hasErrors()) {
+	//		ModelAndView mv = new ModelAndView("adicionando_questoes");
+	//		mv.addObject("listaDificuldade", this.dificuldadeRep.findAll(Sort.by("descricao")));
+	//		mv.addObject("listaAlternativas", this.alternativasRep.findAll(Sort.by("descricao")));
+	//		return mv;
+	//	}
+	//	this.questoesRep.save(questoes);
+	//	return exibirQuestoes();
+	//}
 	
 	@PostMapping("/salvarQuestoes")
-	public String salvarQuestoes(@Valid @ModelAttribute Questoes questoes,Alternativa alternativa, Errors errors, RedirectAttributes ra) {
+	public String salvarQuestoes(@Valid @ModelAttribute Questoes questoes, Errors errors, RedirectAttributes ra) {
 		if (errors.hasErrors()) {
-			ra.addFlashAttribute("mensagemErro", "Não foi possível criar questão: " + errors.getFieldErrors());
+			ra.addFlashAttribute("mensagemErro", "NÃ£o foi possÃ­vel criar questÃ£o: " + errors.getFieldErrors());
 		} else {
 			try {
 				questoesService.criarQuestao(questoes);
-				ra.addFlashAttribute("mensagem", "Questão criada com sucesso");
+				ra.addFlashAttribute("mensagem", "QuestÃ£o [" + questoes.getTitulo() + "] criada com sucesso");
 			} catch (Exception e) {
-				ra.addFlashAttribute("mensagemErro", "Não foi possível criar questão. " + e.getMessage());
+				ra.addFlashAttribute("mensagemErro", "NÃ£o foi possÃ­vel criar questÃ£o. " + e.getMessage());
 			}
 		}
 		return "redirect:/adicionando_questoes";
@@ -90,5 +99,57 @@ public class QuestoesController {
 		return mv;
 	}
 	
+	@PostMapping("/questao")
+	public ModelAndView pesquisarQuestoes(@RequestParam(required=false) String nomePesquisa, RedirectAttributes ra) {
+		ModelAndView mv = new ModelAndView("/questao-achada");		
+		
+		List<Questoes> listaQuestoes;
+		if (nomePesquisa == null || nomePesquisa.trim().isEmpty()) {
+			ra.addFlashAttribute("mensagem", "QuestÃ£o nÃ£o encontrada");
+			listaQuestoes = this.questoesRep.findAll(Sort.by("titulo"));	
+		} else {
+			listaQuestoes = this.questoesRep.findByTituloContainingIgnoreCase(nomePesquisa);
+			//listaQuestoes = this.questoesRep.findByAssunto(nomePesquisa);
+		}
+		
+		mv.addObject("listaQuestoes", listaQuestoes);
+		//mv.addObject("listaAssunto", this.assuntoRep.findAll(Sort.by("descricao")));
+		return mv;
+	}
+	
+	@GetMapping("/questao-achada")
+	public ModelAndView exibirQuestaoAchada(Questoes questoes) {
+		ModelAndView mv = new ModelAndView("/questao-achada");		
+		mv.addObject("listaQuestoes", this.questoesRep.findAll(Sort.by("titulo")));
+		mv.addObject("listaAssunto", this.assuntoRep.findAll(Sort.by("descricao")));
+		mv.addObject("questoes", questoes);
+		return mv;
+	}
+	
+	@GetMapping("/vermais")
+	public ModelAndView verMais(Questoes questoes) {
+		ModelAndView mv = new ModelAndView("/vermais");		
+		mv.addObject("listaQuestoes", this.questoesRep.findAll(Sort.by("titulo")));
+		mv.addObject("listaAssunto", this.assuntoRep.findAll(Sort.by("descricao")));
+		// Caso seja passado o Id do produto, esta chamada será para edição do produto
+		//if (produto != null && produto.getId() != null) {
+			questoes = this.questoesRep.getOne(questoes.getId());
+		//} else { // Caso contrário, será uma adição de novo produto
+		//	produto = new Produto();
+		//}
+		mv.addObject("questoes", questoes);
+		return mv;
+	}
+	
+	@GetMapping("edit/{id}")
+	public ModelAndView exibirEdicao(@PathVariable("id") Integer id) {
+		Questoes questoes = questoesService.findById(id);
+		ModelAndView mv = new ModelAndView("questao-achada");
+		mv.addObject("listaQuestoes", questoesService.listarTodos());
+		mv.addObject("questoes", questoes);
+		return mv;
+	}
+	
 
 }
+
--- src/main/java/br/ifpe/web/projeto2/service/QuestoesService.java
+++ src/main/java/br/ifpe/web/projeto2/service/QuestoesService.java
@@ -1,14 +1,13 @@
 package br.ifpe.web.projeto2.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import br.ifpe.web.projeto2.DAO.AlternativaDAO;
-import br.ifpe.web.projeto2.DAO.AssuntoDAO;
 import br.ifpe.web.projeto2.DAO.QuestoesDAO;
-import br.ifpe.web.projeto2.Model.Alternativa;
 import br.ifpe.web.projeto2.Model.Assunto;
 import br.ifpe.web.projeto2.Model.Questoes;
 import br.ifpe.web.projeto2.exceptions.EnunciadoExistsException;
@@ -21,35 +20,48 @@ public class QuestoesService {
 	
 	@Autowired
 	private QuestoesDAO questoesDAO; 
-	@Autowired AlternativaDAO alternativaDAO;
-	@Autowired AssuntoDAO assuntoDAO;
 	
 	public void criarQuestao(Questoes questoes) throws Exception {
 		
 	    if (this.findQuestoesByEnunciado(questoes.getEnunciado()) != null) {
 	        throw new EnunciadoExistsException
-	          ("Já existe questão com este enunciado: " + questoes.getEnunciado());
-	//    } else if(this.findQuestoesByTitulo(questoes.getTitulo()) != null) {
-	    //	throw new TituloExistsException
-	  //        ("Já existe questão com este titulo: " + questoes.getTitulo());
+	          ("JÃ¡ existe questÃ£o com este enunciado: " + questoes.getEnunciado());
+	    } else if(this.findQuestoesByTitulo(questoes.getTitulo()) != null) {
+	    	throw new TituloExistsException
+	          ("JÃ¡ existe questÃ£o com este titulo: " + questoes.getTitulo());
 	    }
 //	    usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
 	 
 		questoesDAO.save(questoes);
 	}
 	
-	public void criarAssunto(Assunto assunto) {
-		assuntoDAO.save(assunto);
-	}
-	
-	public void criarAlternativa(Alternativa alternativa) {
-		alternativaDAO.save(alternativa);
-	}
-	
 	public Questoes findQuestoesByEnunciado(String enunciado) {
 		return questoesDAO.findByEnunciado(enunciado);
 	}
 	
+	public Object findQuestoesByEnunciadoAprox(String titulo, String enunciado) {
+		return questoesDAO.findByEnunciadoAprox(titulo, enunciado);
+	}
 	
+	public Questoes findQuestoesByTitulo(String titulo) {
+		return questoesDAO.findByTitulo(titulo);
+	}
+	
+	public Questoes findById(Integer id) {
+		return questoesDAO.findById(id).orElse(null);
+	}
+	
+	public List<Questoes> listarTodos() {
+		return questoesDAO.findAll();
+	}
 
+	public Questoes getOne(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public List<Questoes> findQuestoesByAssunto(Assunto assunto){
+		return questoesDAO.findByAssunto(assunto);
+	}
+		
 }
